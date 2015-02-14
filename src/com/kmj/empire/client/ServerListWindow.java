8fd066ce1c95489c69b58b8d21b350e9d02ef39b
Marshall Clyburn <marshall@mardev.net>
2015-02-13 22:35:35 -0500

    Make ServerListWindow extend JFrame.

--- src/com/kmj/empire/client/ServerListWindow.java
+++ src/com/kmj/empire/client/ServerListWindow.java
@@ -1,10 +1,12 @@
 package com.kmj.empire.client;
 
+import javax.swing.JFrame;
+
 // A window that displays a list of games present
 // on a server. The session ID should be set before
 // the client queries the server.
 
-public class ServerListWindow {
+public class ServerListWindow extends JFrame {
 
 	protected int sessionId;
 
