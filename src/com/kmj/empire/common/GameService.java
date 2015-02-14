cea779ce1c4e5c283578327be9c0e7bc9154704e
Marshall Clyburn <marshall@mardev.net>
2015-02-13 21:07:56 -0500

    Correct comment.

--- src/com/kmj/empire/common/GameService.java
+++ src/com/kmj/empire/common/GameService.java
@@ -3,7 +3,7 @@ package com.kmj.empire.common;
 public interface GameService {
 
 	// Verify authentication with the game server.
-	// Returns 0 on success; throws exception on failure.
+	// Returns session ID on success; throws exception on failure.
 	public int authenticate(String username, String password) throws AuthenticationFailedException, ConnectionFailedException;
 
 }
