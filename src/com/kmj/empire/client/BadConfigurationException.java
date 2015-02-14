0f1373b98b9237c03c4495e5a31adba64657f8bb
Marshall Clyburn <marshall@mardev.net>
2015-02-13 21:33:16 -0500

    Add BadConfigurationException.

---
+++ src/com/kmj/empire/client/BadConfigurationException.java
@@ -0,0 +1,12 @@
+package com.kmj.empire.client;
+
+public class BadConfigurationException extends Exception {
+
+	public BadConfigurationException() {
+		super();
+	}
+	
+	public BadConfigurationException(String message) {
+		super(message);
+	}
+}
