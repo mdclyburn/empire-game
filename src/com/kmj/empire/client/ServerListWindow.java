504f002e36f7f0f359c7e9d9611961a6ccb13829
Marshall Clyburn <marshall@mardev.net>
2015-02-13 22:29:16 -0500

    Create ServerListWindow class.
    This is to be where the rest 'Connect To Server' use case is to
    be implemented.

---
+++ src/com/kmj/empire/client/ServerListWindow.java
@@ -0,0 +1,14 @@
+package com.kmj.empire.client;
+
+public class ServerListWindow {
+
+	protected int sessionId;
+
+	public ServerListWindow() {
+		sessionId = -1;
+	}
+	
+	public ServerListWindow(int sessionId) {
+		this.sessionId = sessionId;
+	}
+}
