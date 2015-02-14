d9d9bd35c852730fcd10ba8a4c6adc49d1fafd21
Marshall Clyburn <marshall@mardev.net>
2015-02-14 13:29:03 -0500

    Create AbstractTableModel impementation.
    This is for the representation of active games in a JTable.

---
+++ src/com/kmj/empire/client/GameListTableModel.java
@@ -0,0 +1,47 @@
+package com.kmj.empire.client;
+
+import java.util.ArrayList;
+
+import javax.swing.table.AbstractTableModel;
+
+import com.kmj.empire.common.Game;
+
+public class GameListTableModel extends AbstractTableModel {
+	
+	public static final int COLUMN_NAME = 0;
+	public static final int COLUMN_PLAYERS = 1;
+	
+	ArrayList<Game> games;
+	
+	GameListTableModel() {
+		
+	}
+	
+	void setTableSource(ArrayList<Game> games) { this.games = games; }
+
+	@Override
+	public int getRowCount() {
+		return games.size();
+	}
+
+	@Override
+	public int getColumnCount() {
+		// COLUMN	DESCRIPTION
+		// ====================
+		// 0		The given name for an active game.
+		// 1		The number of players in an active game.
+		return 2;
+	}
+
+	@Override
+	public Object getValueAt(int row, int column) {
+		switch(column) {
+		case COLUMN_NAME: return games.get(row).getName();
+		case COLUMN_PLAYERS: return games.get(row).getActivePlayers();
+		default:
+			System.out.println("Invalid row-column query: " + row + "-" + column + ".");
+			return null;
+		}
+	}
+
+}
