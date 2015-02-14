ba6c482ba2375658cc364e3eb7fd72f2c7a3c4bb
Marshall Clyburn <marshall@mardev.net>
2015-02-14 12:37:46 -0500

    Add function to interface.
    Description of function is in commenting.

--- src/com/kmj/empire/common/GameService.java
+++ src/com/kmj/empire/common/GameService.java
@@ -1,9 +1,14 @@
 package com.kmj.empire.common;
 
+import java.util.ArrayList;
+
 public interface GameService {
 
 	// Verify authentication with the game server.
 	// Returns session ID on success; throws exception on failure.
 	public int authenticate(String username, String password) throws AuthenticationFailedException, ConnectionFailedException;
 
+	// Get the list of active games from the server.
+	// Returns an ArrayList on success; throws exception on failure.
+	public ArrayList<Game> getGameList() throws ConnectionFailedException;
 }
