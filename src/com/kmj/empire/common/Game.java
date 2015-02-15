dd27ebbe0b2a8ba78e803f6ffba8ae7ab04fb1c9
MagicMantis <joesavold@hotmail.com>
2015-02-15 18:54:29 -0500

    Organized Classes

--- src/com/kmj/empire/common/Game.java
+++ src/com/kmj/empire/common/Game.java
@@ -1,17 +1,34 @@
-package com.kmj.empire.common;
+package com.kmj.empire.server;
 
 public class Game {
-	
+
 	protected String name;
 	protected int activePlayers;
-
+	
+	private Sector[][] sectorGrid;
+	private UniverseType universeType;
+	
 	public Game() {
+		this.sectorGrid = new Sector[8][8];
 		name = "Empire Session";
-		activePlayers = 0;
 	}
 	
-	public Game(String name) {
+	public Game(String name, UniverseType universeType) {
+		this();
+		this.universeType = universeType;
 		this.name = name;
+	}
+	
+	public UniverseType getUniverseType() {
+		return universeType;
+	}
+	
+	public Sector[][] getSectorGrid() {
+		return sectorGrid;
+	}
+	
+	public Sector getSector(int x, int y) {
+		return sectorGrid[x][y];
 	}
 	
 	public String getName() { return name; }
--- src/com/kmj/empire/common/GameService.java
+++ src/com/kmj/empire/common/GameService.java
@@ -1,15 +1,28 @@
-package com.kmj.empire.common;
+package com.kmj.empire.server;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public interface GameService {
+	
+	
+	/* Restore a saved game to the server */
+	public int restoreGame(Game game) throws ConnectionFailedException;
+	
+	/* Returns the details/state of the game identified by gameId */
+	public Game getGameState(int gameId) throws ConnectionFailedException;
+	
+	/* Returns a list of ongoing games */
+	public ArrayList<Game> getGamesList(int sessionId) throws AuthenticationFailedException, ConnectionFailedException;
+	
+	/* Authenticate username and password, returns playerID */
+	public int authenticate(String user, String password) throws ConnectionFailedException;
+	
+	/* Creates a game on this server */
+	public int createGame() throws ConnectionFailedException;
+	
+	/* Join an existing game on this server */
+	public void joinGame() throws ConnectionFailedException;
 
-	// Verify authentication with the game server.
-	// Returns session ID on success; throws exception on failure.
-	public int authenticate(String username, String password) throws AuthenticationFailedException, ConnectionFailedException;
-
-	// Get the list of active games from the server.
-	// Returns an ArrayList on success; throws exception on failure.
-	public ArrayList<Game> getGameList(int sessionId) throws ConnectionFailedException;
-
+	
+	
 }
