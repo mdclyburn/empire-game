a2a0461b1149efa7c2951acccd0cf60863f221c3
Marshall Clyburn <marshall@mardev.net>
2015-02-16 11:51:06 -0500

    Draw box around selected universe sector.

--- src/com/kmj/empire/client/UniverseView.java
+++ src/com/kmj/empire/client/UniverseView.java
@@ -2,24 +2,32 @@ package com.kmj.empire.client;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.JPanel;
 
 import com.kmj.empire.common.Game;
+import com.kmj.empire.common.Location;
 
-public class UniverseView extends JPanel {
+public class UniverseView extends JPanel implements MouseListener {
 	
 	protected Game game;
+	protected Location selectedSector;
 	
 	protected static final int PADDING = 5;
 
 	public UniverseView() {
 		super();
+		selectedSector = new Location();
+		addMouseListener(this);
 	}
 	
 	public UniverseView(Game game) {
 		super();
 		this.game = game;
+		selectedSector = new Location();
+		addMouseListener(this);
 	}
 	
 	@Override
@@ -37,7 +45,32 @@ public class UniverseView extends JPanel {
 			g.drawLine(0, i * getHeight() / 8, getWidth(), i * getHeight() / 8);
 		}
 		
+		// Draw box around selected sector
+		g.setColor(Color.RED);
+		Location topLeft = new Location();
+		Location topRight = new Location();
+		Location bottomLeft = new Location();
+		Location bottomRight = new Location();
+		
+		topLeft.x = (selectedSector.x - 1) * getWidth() / 8;
+		topLeft.y = (selectedSector.y - 1) * getHeight() / 8;
+		
+		topRight.x = selectedSector.x * getWidth() / 8;
+		topRight.y = topLeft.y;
+		
+		bottomLeft.x = topLeft.x;
+		bottomLeft.y = selectedSector.y * getHeight() / 8;
+		
+		bottomRight.x = topRight.x;
+		bottomRight.y = bottomLeft.y;
+		
+		g.drawLine(topLeft.x, topLeft.y, topRight.x, topRight.y);
+		g.drawLine(topLeft.x, topLeft.y, bottomLeft.x, bottomLeft.y);
+		g.drawLine(bottomLeft.x, bottomLeft.y, bottomRight.x, bottomRight.y);
+		g.drawLine(bottomRight.x, bottomRight.y, topRight.x, topRight.y);
+		
 		// Display information.
+		g.setColor(Color.WHITE);
 		for(int y = 0; y < 8; y++) {
 			for(int x = 0; x < 8; x++) {
 				g.drawString(Integer.toString(game.getSector(x, y).getPlanets()), (x * getWidth() / 8) + PADDING, (y * getHeight() / 8) + (3 * PADDING));
@@ -46,4 +79,25 @@ public class UniverseView extends JPanel {
 			}
 		}
 	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		selectedSector.x = (e.getX() / (getWidth() / 8)) + 1;
+		selectedSector.y = (e.getY() / (getHeight() / 8)) + 1;
+		repaint();
+		
+		return;
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {}
+
+	@Override
+	public void mouseExited(MouseEvent e) {}
 }
