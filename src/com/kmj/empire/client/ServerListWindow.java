1133bb384171487163fa2ce9787626bd57a89b3d
Marshall Clyburn <marshall@mardev.net>
2015-02-18 12:52:05 -0500

    Remove JUnit imports.

--- src/com/kmj/empire/client/ServerListWindow.java
+++ src/com/kmj/empire/client/ServerListWindow.java
@@ -1,8 +1,5 @@
 package com.kmj.empire.client;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.fail;
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
