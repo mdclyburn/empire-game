be951b806384556b49f492d8aa48b80338a40b72
Marshall Clyburn <marshall@mardev.net>
2015-03-04 15:28:14 -0500

    Comment SessionObserver.

--- src/com/kmj/empire/client/controller/SessionObserver.java
+++ src/com/kmj/empire/client/controller/SessionObserver.java
@@ -2,19 +2,23 @@ package com.kmj.empire.client.controller;
 
 import com.kmj.empire.common.Game;
 
-/*
+/**
  * Any class implementing this interface can be
  * notified whenever the session ID or the state of the
  * Game changes.
  */
 public interface SessionObserver {
 	
-	// The action to be taken when the session ID has been
-	// set or changed.
+	/**
+	 * The method that is called when the session ID has changed.
+	 * @param newId The new ID
+	 */
 	public void onIdChanged(int newId);
 	
-	// The action to be taken when the Game object has been
-	// changed.
+	/**
+	 * The method that is called when the Game has been updated.
+	 * @param newGame The new Game
+	 */
 	public void onGameChanged(Game newGame);
 
 }
